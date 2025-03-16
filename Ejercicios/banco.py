68aa7c9e14aac1743ac523c982a1c4dac95781c8
httpsmarioooo <111519152+httpsmarioooo@users.noreply.github.com>
2025-03-16 02:47:55 -0500

    Update Banco

--- Ejercicios/banco.py
+++ Ejercicios/banco.py
@@ -7,26 +7,29 @@ while True:
     print("3. Consultar saldo")
     print("4. Salir")
 
-    opcion = int(input("Ingrese una opcion: "))
+    try:
+        opcion = int(input("Ingrese una opción: "))
 
-    if opcion == 1: 
-        cantidad = float(input("Ingrese la cantidad  a depositar: "))
-        saldo += cantidad
-        print(f"Deposito exitoso. Saldo Actual: {saldo}")
+        if opcion == 1:
+            cantidad = float(input("Ingrese la cantidad a depositar: "))
+            saldo += cantidad
+            print(f"Depósito exitoso. Saldo actual: {saldo}")
 
-    elif opcion == 2:
-        cantidad = float(input("Ingrese la cantidad a retirar: "))
-        if cantidad <= saldo:
-            saldo -= cantidad
-            print (f"Retiro exitoso. Saldo actual: {saldo}")
+        elif opcion == 2:
+            cantidad = float(input("Ingrese la cantidad a retirar: "))
+            if cantidad <= saldo:
+                saldo -= cantidad
+                print(f"Retiro exitoso. Saldo actual: {saldo}")
+            else:
+                print("Fondos insuficientes.")
+
+        elif opcion == 3:
+            print(f"Este es tu saldo: {saldo}")
+
+        elif opcion == 4:
+            break
         else:
-            print("Fondos insuficientes.")
-
-    elif opcion == 3:
-        mostrarSaldo = print(f"Este es tu saldo: {saldo}")
-    
-    elif opcion == 4:
-        break
-
-    else:
-        print("Opcion invalida")
+            print ("Opción inválida")
+        
+    except ValueError:
+        print("Error: Ingrese un número válido.")
