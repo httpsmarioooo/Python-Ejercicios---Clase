a214cf683b5d5eea926245e451ad1ebff7964209
httpsmarioooo <111519152+httpsmarioooo@users.noreply.github.com>
2025-03-17 09:27:13 -0500

    Update FizzBuzz

--- Ejercicios/fizzbuzz.py
+++ Ejercicios/fizzbuzz.py
@@ -1,29 +1,29 @@
 # Números esperados (múltiplos de 3 o 5)
 
-while True:
-    print("Introduce un número para comprobar si es múltiplo de 3 o 5: ")
-    try:
-        numero = int(input())
-        if numero % 3 == 0 or numero % 5 == 0:
-            # Si es múltiplo de 3 o 5, mostrar el resultado
-            if numero % 3 == 0 and numero % 5 == 0:
-                print("FizzBuzz")
-            elif numero % 3 == 0:
-                print("Fizz")
-            elif numero % 5 == 0:
-                print("Buzz")
-            break
-    except ValueError:
-        print("No has ingresado un número. Por favor, inténtalo de nuevo.")
+# while True:
+#     print("Introduce un número para comprobar si es múltiplo de 3 o 5: ")
+#     try:
+#         numero = int(input())
+#         if numero % 3 == 0 or numero % 5 == 0:
+#             # Si es múltiplo de 3 o 5, mostrar el resultado
+#             if numero % 3 == 0 and numero % 5 == 0:
+#                 print("FizzBuzz")
+#             elif numero % 3 == 0:
+#                 print("Fizz")
+#             elif numero % 5 == 0:
+#                 print("Buzz")
+#             break
+#     except ValueError:
+#         print("No has ingresado un número. Por favor, inténtalo de nuevo.")
 
 
-# Generar una secuencia de números del 1 al 21
-# for numero in range(1, 21):
-#     if numero % 3 == 0 and numero % 5 == 0:
-#         print("FizzBuzz")
-#     elif numero % 3 == 0:
-#         print("Fizz")
-#     elif numero % 5 == 0:
-#         print("Buzz")
-#     else:
-#         print(numero)
+# Generar una secuencia de números del 1 al 100
+for numero in range(1, 101):
+    if numero % 3 == 0 and numero % 5 == 0:
+        print("FizzBuzz")
+    elif numero % 3 == 0:
+        print("Fizz")
+    elif numero % 5 == 0:
+        print("Buzz")
+    else:
+        print(numero)
